fb9d19aab97325e3ae20424f1f1bfcbc55ba0eb0
Kinga Sieminiak <kinga.sieminiak@elpassion.pl>
2019-02-16 22:23:27 +0100

    Prevent modifying an existing user

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,3 +1,5 @@
+import { navigate } from '@reach/router';
+
 export const signInUser = () => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     const firebase = getFirebase();
@@ -5,20 +7,32 @@ export const signInUser = () => {
     const authProvider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithPopup(authProvider).then((resp) => {
-      firestore.collection('users').doc(resp.user.uid).set({
-        name: resp.user.displayName,
-        email: resp.user.email,
-        address: 'address',
-        passport: '111'
+      const userFromCollection = firestore.collection('users').doc(resp.user.uid).get();
+
+      userFromCollection.then((doc) => {
+        if (!doc.data()) {
+          firestore.collection('users').doc(resp.user.uid).set({
+            name: resp.user.displayName,
+            email: resp.user.email,
+            photoURL: resp.user.photoURL,
+            updated: new Date(),
+            sex: resp.additionalUserInfo.profile.gender,
+            address: null,
+            IDnumber: null,
+            reports: [],
+            msisdn: null,
+            number: resp.user.phoneNumber,
+          });
+        }
+      }).then(() => {
+        dispatch({ type: 'LOGIN_USER', uid: 111, isUserAutorized: true });
+        navigate('/');
+
+      }).catch(error => {
+        console.log(error);
       });
-
-    }).then(() => {
-      dispatch({ type: 'LOGIN_USER', uid: 111, isUserAutorized: true });
-
     }).catch((error) => {
-      // TO DO dispatch error
-
-      const errorCode = error.code;
+      const errorCode = error.code; // TO DO dispatch error
       const errorMessage = error.message;
       const email = error.email;
       const credential = error.credential;
