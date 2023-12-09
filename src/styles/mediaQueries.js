cb47c54ce60bba51615dfa0853ef2516915877e3
Kinga Sieminiak <kinga.sieminiak@zalando.de>
2023-12-09 19:21:39 +0100

    Fix test config

--- src/styles/mediaQueries.js
+++ src/styles/mediaQueries.js
@@ -3,7 +3,7 @@ export const breakpoints = {
   md: '700',
   lg: '1200',
   xl: '1440',
-}
+};
 
 const mediaMin = size => `@media screen and (min-width: ${size}px)`;
 export const mediaMax = size => `@media screen and (max-width: ${size}px)`;
