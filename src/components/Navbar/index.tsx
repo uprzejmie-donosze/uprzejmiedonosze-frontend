74f9910c82b86ecf303af0a307af6793c7505313
Kinga Sieminiak <kinga.sieminiak@zalando.de>
2023-12-20 16:47:10 +0100

    Improve logic of Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { closeNavbar, openNavbar } from "../../store/app";
 import { useAppDispatch, useAppSelector } from "../../store";
 import { LinearLoader } from "../Loader";
-import { UserIcon } from "../Icons";
+import { BurgerIcon, UserIcon } from "../Icons";
 import { ROUTES } from "../../config";
 import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
@@ -29,18 +29,12 @@ function Navbar() {
   return (
     <S.Navbar>
       <S.Container>
-        <S.Logo to={ROUTES.home}>Uprzejmie Donoszę</S.Logo>
+        <S.Logo onClick={closeNav} to={ROUTES.home}>
+          Uprzejmie Donoszę
+        </S.Logo>
 
         <S.Burger onClick={toggleMenu}>
-          <svg width="20px" height="20px" viewBox="0 0 92 92">
-            <path
-              fill="currentColor"
-              id="XMLID_101_"
-              d="M78,23.5H14c-3.6,0-6.5-2.9-6.5-6.5s2.9-6.5,6.5-6.5h64c3.6,0,6.5,2.9,6.5,6.5S81.6,23.5,78,23.5z M84.5,46
-            c0-3.6-2.9-6.5-6.5-6.5H14c-3.6,0-6.5,2.9-6.5,6.5s2.9,6.5,6.5,6.5h64C81.6,52.5,84.5,49.6,84.5,46z M84.5,75c0-3.6-2.9-6.5-6.5-6.5
-            H14c-3.6,0-6.5,2.9-6.5,6.5s2.9,6.5,6.5,6.5h64C81.6,81.5,84.5,78.6,84.5,75z"
-            />
-          </svg>
+          <BurgerIcon />
         </S.Burger>
 
         <S.Overlay onClick={toggleMenu} isNavOpened={isNavOpened} />
@@ -55,8 +49,10 @@ function Navbar() {
 
 function Menu({ closeNav }: { closeNav: () => void }) {
   const user = useAppSelector((state) => state.user);
-  let photoURL = useAppSelector((state) => state.firebase.auth.photoURL);
-  photoURL = user.isEmpty ? "" : photoURL;
+  const photoURL = useAppSelector((state) => state.firebase.auth.photoURL);
+
+  const photo =
+    user.isEmpty || !photoURL ? <UserIcon /> : <S.Photo src={photoURL} />;
 
   if (!user.isLoaded) {
     return <LinearLoader />;
@@ -66,9 +62,7 @@ function Menu({ closeNav }: { closeNav: () => void }) {
     return (
       <>
         <S.Header>
-          <S.Avatar>
-            {!!photoURL ? <S.Photo src={photoURL} /> : <UserIcon />}
-          </S.Avatar>
+          <S.Avatar>{photo}</S.Avatar>
 
           <S.Title onClick={closeNav} to={ROUTES.login}>
             Zaloguj/ zarejestruj się
@@ -85,11 +79,10 @@ function Menu({ closeNav }: { closeNav: () => void }) {
   return (
     <>
       <S.Header>
-        <S.Avatar>
-          {!!photoURL ? <S.Photo src={photoURL} /> : <UserIcon />}
-        </S.Avatar>
-
-        <S.Title to={ROUTES.user.main}>{user.profile.data.name}</S.Title>
+        <S.Avatar>{photo}</S.Avatar>
+        <S.Title onClick={closeNav} to={ROUTES.user.main}>
+          {user.profile.data.name}
+        </S.Title>
       </S.Header>
 
       <S.Body>
