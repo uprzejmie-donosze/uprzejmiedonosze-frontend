1238e2fe54117c44abf60c863c29029cbdd7952f
Kinga Sieminiak <kinga.sieminiak@zalando.de>
2023-12-22 21:49:42 +0100

    Remove unused code

--- src/components/Form/validation.ts
+++ src/components/Form/validation.ts
@@ -3,14 +3,4 @@ const required = (type: string, message?: string) => (value: any) =>
     ? undefined
     : message || "Pole wymagane";
 
-const validateByPattern = (pattern: RegExp) => (value: any) => {
-  if (value !== undefined) {
-    return value.match(pattern) ? undefined : "Must follow pattern key=value";
-  }
-  return undefined;
-};
-
 export const stringRequired = required("string");
-export const camparisonPattern = validateByPattern(
-  /^[a-zA-Z0-9]+=[a-zA-Z0-9%-_]+$/,
-);
