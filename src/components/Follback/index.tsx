7f8d1d173ae1f45b9159fefa6873562d7c057671
Kinga Sieminiak <kinga.sieminiak@zalando.de>
2023-12-26 13:30:22 +0100

    Remove unused import

--- src/components/Follback/index.tsx
+++ src/components/Follback/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../store";
 import { Toast } from "./styles";
 import { cleanError } from "../../store/follback/follbackActions";
