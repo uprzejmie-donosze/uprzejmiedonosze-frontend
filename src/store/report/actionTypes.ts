dac7df6abf741808f620851ebe14221e908df9cd
Kinga Sieminiak <kinga.sieminiak@zalando.de>
2023-12-29 11:30:35 +0100

    Add report reducer

---
+++ src/store/report/actionTypes.ts
@@ -0,0 +1,3 @@
+export const REPORT_ACTIONS = {
+  clean: "REPORT/CLEAN",
+};
---
+++ src/store/report/reportActions.ts
@@ -0,0 +1,5 @@
+import { REPORT_ACTIONS } from "./actionTypes";
+
+export function clean() {
+  return { type: REPORT_ACTIONS.clean };
+}
