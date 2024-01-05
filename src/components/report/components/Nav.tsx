cbe90657fd0397d597048d8acec89d5c026e0e2d
Kinga Sieminiak <kinga.sieminiak@zalando.de>
2024-01-05 16:09:47 +0100

    Fix styles

--- src/components/report/components/Nav.tsx
+++ src/components/report/components/Nav.tsx
@@ -16,7 +16,8 @@ const NavContainer = styled.div`
   position: fixed;
   bottom: 0;
   right: 0;
+  z-index: 100;
   width: 100%;
   background: white;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+  box-shadow: 0px -3px 7px rgb(51 51 51 / 16%);
 `;
