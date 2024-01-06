a5575e1098fd4dc5e5db1db85ee581a4cfdcedc0
Kinga Sieminiak <kinga.sieminiak@zalando.de>
2024-01-06 19:11:38 +0100

    Improve report response type

--- src/api/responses.ts
+++ src/api/responses.ts
@@ -1,22 +1,24 @@
 export type IUser = {
-  data: {
-    name?: string;
-    msisdn?: string;
-    address?: string;
-    email: string;
-    sex?: string;
-    exposeData?: boolean;
-    stopAgresji?: boolean;
-    termsConfirmation?: string;
-    autoSend?: boolean;
-    myAppsSize?: number;
-  };
+  data: UserData;
   number: number;
   updated?: string;
   lastLocation?: string;
   appsCount?: number;
   isRegistered?: boolean;
   isTermsConfirmed?: boolean;
+};
+
+export type UserData = {
+  name?: string;
+  msisdn?: string;
+  address?: string;
+  email: string;
+  sex?: string;
+  exposeData?: boolean;
+  stopAgresji?: boolean;
+  termsConfirmation?: string;
+  autoSend?: boolean;
+  myAppsSize?: number;
 };
 
 export type ErrorResponse = {
@@ -26,25 +28,57 @@ export type ErrorResponse = {
   reson?: string; // only in staging env for debug/tracebility
 };
 
-export type NewReport = {
+export type Report = {
   loaded: boolean;
   loading: boolean;
   date: string | null;
   id: string;
   added: string | null;
-  user: {}; //TOOD
+  user: UserData;
   status: string;
   category: number;
   statements: {
     witness: boolean;
     gallery: boolean;
-    hideNameInPdf: boolean;
   };
-  address: {}; //TOOD
+  address: {
+    latlng?: string;
+    address?: string;
+    city?: string;
+    voivodeship?: string;
+    district?: string;
+    mapImage?: string;
+    county?: string;
+    municipality?: string;
+    postcode?: string;
+  };
   version: string;
-  statusHistory: []; //TOOD
-  comments: []; // TODO
-  extensions: []; //TODO
+  statusHistory: [
+    {
+      [key: string]: {
+        old: string;
+        new: string;
+      };
+    },
+  ];
+  comments: [
+    {
+      [key: string]: {
+        source: string;
+        comment: string;
+      };
+    },
+  ];
+  extensions: [
+    {
+      [key: string]: {
+        law: string;
+        price: string;
+        stopAgresjiOnly: string;
+        title: string;
+      };
+    },
+  ];
   carImage?: {
     url: string;
     thumb: string;
@@ -63,5 +97,20 @@ export type NewReport = {
     colorConfidence: number;
     recydywa: number;
   };
-  alpr: string;
+  alpr?: string;
+  smCity?: string;
+  userComment?: string;
+  stopAgresji?: boolean;
+  seq?: number;
+  number?: string;
+  sent?: {
+    date?: string;
+    subject?: string;
+    to?: string;
+    method?: string;
+    cc?: string;
+    from?: string;
+    body?: string;
+    curl?: object;
+  };
 };
