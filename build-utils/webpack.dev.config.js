9e11e04400c05ce67512fec5f3617d75a20771d7
SZN <szymon@nieradka.net>
2024-01-10 15:54:30 +0100

    Allow webpack-dev-server to server on any-host

--- build-utils/webpack.dev.config.js
+++ build-utils/webpack.dev.config.js
@@ -1,28 +1,29 @@
-const ESLintPlugin = require('eslint-webpack-plugin');
-const webpack = require('webpack');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 const config = {
-  mode: 'development',
+  mode: "development",
   plugins: [
     new ESLintPlugin({
       failOnError: false,
       emitError: true,
       emitWarning: true,
-    })
+    }),
   ],
   devServer: {
     historyApiFallback: true,
     static: {
-      directory: './public',
+      directory: "./public",
     },
+    allowedHosts: "all",
     proxy: {
-      '/api/*': {
-          target: process.env.PROXY || 'http://localhost:8080',
-          changeOrigin: true,
-          secure: true
-      }
-    }
-  }
-}
+      "/api/*": {
+        target: process.env.PROXY || "http://localhost:8080",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+};
 
 module.exports = config;
